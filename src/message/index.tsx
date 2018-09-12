56a4367b3588b0ce9bb6b94ceb16080af9701c46
Jonas W <jonaswilms2000@gmail.com>
2018-09-12 20:03:17 +0200

    feat: Add TextArea
    The user can input what he wants to, thats quite good for feedback

--- src/message/index.tsx
+++ src/message/index.tsx
@@ -4,6 +4,7 @@ import {Notification, NotificationUI, isNotification} from "./notification";
 import {MultipleChoice, MultipleChoiceUI, isMultipleChoice} from "./multiplechoice";
 import {InputChoice, InputChoiceUI, isInputChoice} from "./inputchoice";
 import {Image, ImageUI, isImage} from "./image";
+import {TextArea, TextAreaUI, isTextArea} from "./textarea";
 
 export interface Message {
   id: string;
@@ -18,6 +19,9 @@ export interface Props {
 
 
 export const MessageUI = (props: Message & Props) => {
+  if(isTextArea(props))
+    return <TextAreaUI {...props} />;
+    
   if(isMultipleChoice(props))
     return <MultipleChoiceUI {...props} />;
 
